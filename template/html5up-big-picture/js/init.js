1efa58580f4c315d710f1ec623b4ff97c12463f9
Øistein Sørensen <sorenso@gmail.com>
2014-09-03 22:21:37 +0200

    Template adjustments.

--- template/html5up-big-picture/js/init.js
+++ template/html5up-big-picture/js/init.js
@@ -22,12 +22,12 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			'max': { range: '*', href: 'css/style.css', containers: 1440, viewport: { scalable: false }, grid: { gutters: 40 } },
-			'wide': { range: '-1920', href: 'css/style-wide.css', containers: 1360 },
-			'normal': { range: '-1680', href: 'css/style-normal.css', containers: 1200 },
-			'narrow': { range: '-1280', href: 'css/style-narrow.css', containers: 960 },
-			'narrower': { range: '-1000', href: 'css/style-narrower.css', containers: '95%' },
-			'mobile': { range: '-640', href: 'css/style-mobile.css', grid: { gutters: 20 } },
+			'max': { range: '*', href: '/css/style.css', containers: 1440, viewport: { scalable: false }, grid: { gutters: 40 } },
+			'wide': { range: '-1920', href: '/css/style-wide.css', containers: 1360 },
+			'normal': { range: '-1680', href: '/css/style-normal.css', containers: 1200 },
+			'narrow': { range: '-1280', href: '/css/style-narrow.css', containers: 960 },
+			'narrower': { range: '-1000', href: '/css/style-narrower.css', containers: '95%' },
+			'mobile': { range: '-640', href: '/css/style-mobile.css', grid: { gutters: 20 } },
 			'mobile-narrow': { range: '-480', grid: { collapse: true, gutters: 10 } }
 		}
 	});
