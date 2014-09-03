78784fa9e566b77dafcee2ad6815c89786fb6b07
Øistein Sørensen <sorenso@gmail.com>
2014-09-03 18:49:34 +0200

    Added auto loading of the newest file when index.md is missing

--- app/routes/web.js
+++ app/routes/web.js
@@ -16,7 +16,7 @@ var express    = require('express'),
     path          = require('path'),
     app_path      = __dirname + '/../../',
     template_path = path.normalize(app_path + 'template/current/'),
-    content_path  = path.normalize(app_path + 'content/articles'),
+    content_path  = path.normalize(app_path + 'content/articles/'),
     logger        = require(app_path + 'lib/logger')();
 
 var web_router = express.Router();
@@ -42,24 +42,47 @@ web_router.get('/favicon.ico', function (req, res) {
     res.send('hola');
 });
 
+
+function getArticlePathRelative (complete_filename) {
+    console.log(complete_filename);
+    var re = new RegExp(content_path);
+    var relative_path = complete_filename.replace(re, '');
+    // Remove filename
+    relative_path = relative_path.replace(/(\/*[^\/]+$)/, '');
+    console.log(relative_path);
+    return relative_path ? '/' + relative_path : '';
+}
+function getUrlFromRequest (req) {
+    var article_filename = req.url.replace(/\//, '');
+    article_filename = decodeURIComponent(article_filename);
+    article_filename = article_filename.replace(/\.html/, '');
+    return article_filename;
+}
+function getMarkdownFilename (article_filename) {
+    var markdown_filename = path.normalize(content_path + article_filename + '.md');
+//    markdown_filename = markdown_filename.replace(/\/.md$/, '/index.md');
+    return markdown_filename;
+}
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 web_router.get('/*', function(req, res) {
     // Resolve filename
-    var article_filename = req.url.replace(/\//, '');
-    article_filename = article_filename.replace(/\.html/, '');
+//    console.log(req.url);
+    var article_filename = getUrlFromRequest(req);
 
     // Check for cached file
     // If not cached compile file and store it.
     var tpl = swig.compileFile(template_path + 'blog.html');
 
     // Load content based on filename
-    var markdown_filename = path.normalize(app_path + 'content/articles/' + article_filename + '.md');
-    markdown_filename = markdown_filename.replace(/\/.md$/, '/index.md');
+    var markdown_filename = getMarkdownFilename(article_filename);
+    var article_path = getArticlePathRelative(markdown_filename);
 
     // Load from function
     var article = require(app_path + 'lib/article')({
         logger: logger,
         filename: markdown_filename,
+        article_path: article_path,
         content_path: content_path
     });
 
@@ -70,7 +93,7 @@ web_router.get('/*', function(req, res) {
             res.send(tpl({ article: article }));
         })
         .catch(function (opt) {
-            console.log(opt);
+//            console.log(opt);
             res.status(404).send(tpl({ error: opt.error, article: opt.article }));
         })
         .done();
