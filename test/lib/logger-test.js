8bbbcb9177a923f4784f89519dc0e947a35fd17a
Øistein Sørensen <sorenso@gmail.com>
2014-09-18 23:32:52 +0200

    Working on test for logger.js

---
+++ test/lib/logger-test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var buster = require('buster'),
+    assert = buster.assert,
+    refute = buster.refute,
+    article = require('../../lib/logger');
+
+buster.testCase('lib/logger', {
+    setUp: function () {
+    },
+    tearDown: function () {
+    },
+    'Test logger:': {
+        'This is true': function () {
+            assert(true);
+        }
+
+
+
+
+
+    }
+});
