55cb75ff8764427057717b858797ea33a52ccac0
Øistein Sørensen <sorenso@gmail.com>
2014-09-20 01:03:17 +0200

    Added sig proposal as comments.

--- app/server.js
+++ app/server.js
@@ -59,8 +59,31 @@ if (cluster.isMaster) {
     app.use('/', web_router);
 
     // Start the server -------------------------------
-    app.listen(config.app.port);
+    var server = app.listen(config.app.port);
     logger.log('Something happens on port ' + config.app.port);
+
+
+//    // this function is called when you want the server to die gracefully
+//    // i.e. wait for existing connections
+//    var gracefulShutdown = function() {
+//        console.log("Received kill signal, shutting down gracefully.");
+//        server.close(function() {
+//            console.log("Closed out remaining connections.");
+//            process.exit();
+//        });
+//
+//        // if after
+//        setTimeout(function() {
+//            console.error("Could not close connections in time, forcefully shutting down");
+//            process.exit();
+//        }, 10*1000);
+//    };
+//
+//    // listen for TERM signal .e.g. kill
+//    process.on ('SIGTERM', gracefulShutdown);
+//
+//    // listen for INT signal e.g. Ctrl-C
+//    process.on ('SIGINT', gracefulShutdown);
 
 }
 
