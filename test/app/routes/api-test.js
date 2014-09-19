4d6f71f0b15d52d671db309949c5fa03f1632b07
Øistein Sørensen <sorenso@gmail.com>
2014-09-20 01:04:57 +0200

    Started working on tests for http interfaces.

---
+++ test/app/routes/api-test.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var buster     = require('buster'),
+    assert     = buster.assert,
+    refute     = buster.refute,
+    when       = require('when'),
+    express    = require('express'),
+    request    = require('request'),
+    api_router = require('../../../app/routes/api');
+
+var port = 4321;
+var app = express();
+app.use('/api', api_router);
+var server;
+
+var responses = {
+    endpoints : {"message":"hooray! welcome to our api!"}
+};
+
+
+buster.testCase('app/routes/api', {
+    setUp: function () {
+        // TODO: Start webserver and use routes.
+        console.log('api: setup');
+        this.timeout = 2000;
+        server = app.listen(port);
+    },
+    tearDown: function (done) {
+        // TODO: Shutdown webserver.
+        console.log('api: tearDown');
+        server.close(function() {
+            console.log("Closed out remaining connections.");
+//            process.exit();
+            done();
+        });
+    },
+    'Test api routes:': {
+        'endpoints': function (done) {
+            request('http://127.0.0.1:' + port + '/api', done(function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log(body);
+//                    console.log(response);
+                    assert.equals(JSON.parse(body), responses.endpoints);
+                }
+            }));
+
+//            when(article.catlist({}))
+//                .done(function (obj) {
+//                    assert(true);
+//                    done();
+//                });
+        },
+//        'test': function () {
+//            assert(true);
+//        }
+
+
+    }
+});
---
+++ test/app/routes/web-test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var buster     = require('buster'),
+    assert     = buster.assert,
+    refute     = buster.refute,
+    when       = require('when'),
+    express    = require('express'),
+    request    = require('request'),
+    web_router = require('../../../app/routes/web');
+
+var config = require('../../../config/config-dist.js');
+web_router.set_config(config, {
+    content_path: __dirname + '/../../content/articles/'
+});
+
+var port = 4321;
+var app = express();
+app.use('/web', web_router);
+var server;
+
+var responses = {
+    endpoints : {"message":"hooray! welcome to our api!"}
+};
+
+buster.testCase('app/routes/web', {
+    setUp: function () {
+        // TODO: Start webserver and use routes.
+        console.log('web: setup');
+        this.timeout = 2000;
+        server = app.listen(port);
+    },
+    tearDown: function (done) {
+        console.log('web: tearDown');
+        // TODO: Shutdown webserver.
+        server.close(function() {
+            console.log("Closed out remaining connections.");
+//            delete require.cache[require.resolve('../../../lib/article')];
+            done();
+        });
+    },
+    'Test web routes:': {
+        'endpoints': function (done) {
+//            request('http://127.0.0.1:' + port + '/web/simple-blog/index', function (error, response, body) {
+            request('http://127.0.0.1:' + port + '/web/simple-blog/index', function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log(body);
+                    assert(true);
+                    done();
+
+
+//                    console.log(response);
+//                    assert.equals(JSON.parse(body), responses.endpoints);
+                } else {
+                    console.log(response.statusCode);
+                    console.log(body);
+                    done();
+                }
+            });
+
+
+
+
+//            when(article.catlist({}))
+//                .done(function (obj) {
+//                    assert(true);
+//                    done();
+//                });
+        },
+
+
+
+    }
+});
