7435b82b08783663477e1a9db7fc9d8d20d713b4
Øistein Sørensen <sorenso@gmail.com>
2014-09-26 14:05:52 +0200

    Added image tests.

--- test/app/routes/web-test.js
+++ test/app/routes/web-test.js
@@ -4,13 +4,20 @@ var buster     = require('buster'),
     assert     = buster.assert,
     refute     = buster.refute,
     when       = require('when'),
+    fs         = require('fs'),
     express    = require('express'),
     request    = require('request'),
     web_router = require('../../../app/routes/web');
 
+var content_path     = __dirname + '/../../content/articles/',
+    photo_path       = __dirname + '/../../content/images/',
+    photo_cache_path = __dirname + '/../../content/images_cached/';
+
 var config = require('../../../config/config-dist.js');
 web_router.set_config(config, {
-    content_path: __dirname + '/../../content/articles/'
+    content_path: content_path,
+    photo_path: photo_path,
+    photo_cache_path: photo_cache_path
 });
 
 var port = 4321;
@@ -21,6 +28,26 @@ var server;
 var responses = {
     endpoints : {"message":"hooray! welcome to our api!"}
 };
+
+
+var rmDir = function(dirPath) {
+    var files;
+    try { files = fs.readdirSync(dirPath); }
+    catch(e) { return; }
+    if (files.length > 0) {
+        for (var i = 0; i < files.length; i++) {
+            var filePath = dirPath + '/' + files[i];
+            if (fs.statSync(filePath).isFile()) {
+                fs.unlinkSync(filePath);
+            } else {
+                rmDir(filePath);
+            }
+        }
+    }
+    fs.rmdirSync(dirPath);
+};
+
+
 
 buster.testCase('app/routes/web', {
     setUp: function () {
@@ -33,39 +60,68 @@ buster.testCase('app/routes/web', {
 //        console.log('web: tearDown');
         // TODO: Shutdown webserver.
         server.close(function() {
-//            console.log("Closed out remaining connections.");
-//            delete require.cache[require.resolve('../../../lib/article')];
-
             done();
         });
     },
     'Test web routes:': {
         'simple-blog/index': function (done) {
-//            request('http://127.0.0.1:' + port + '/web/simple-blog/index', function (error, response, body) {
             request('http://127.0.0.1:' + port + '/web/simple-blog/index', function (error, response, body) {
                 if (!error && response.statusCode === 200) {
-//                    console.log(body);
                     assert(true);
                     done();
                     assert.equals(200, response.statusCode);
-//                    console.log(response);
-//                    assert.equals(JSON.parse(body), responses.endpoints);
                 } else {
                     console.log('response.statusCode:', response.statusCode);
-//                    console.log(body);
                     done();
                 }
             });
 
-
-
-
-//            when(article.catlist({}))
-//                .done(function (obj) {
-//                    assert(true);
-//                    done();
-//                });
         },
+
+        'pho/test.jpg?w=300 from scratch': function (done) {
+            rmDir(photo_cache_path);
+            request('http://127.0.0.1:' + port + '/web/pho/test.jpg?w=300', function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    done();
+                    assert.equals(200, response.statusCode);
+                } else {
+                    console.log('response.statusCode:', response.statusCode);
+                    console.log(error);
+                    console.log(response);
+                    done();
+                }
+            });
+        },
+
+        'pho/test.jpg?w=300 cached version': function (done) {
+            request('http://127.0.0.1:' + port + '/web/pho/test.jpg?w=300', function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    done();
+                    assert.equals(200, response.statusCode);
+                } else {
+                    console.log('response.statusCode:', response.statusCode);
+                    console.log(error);
+                    console.log(response);
+                    done();
+                }
+            });
+        },
+
+
+        'pho/test.jpg?w=300&force=1 force cache refresh': function (done) {
+            request('http://127.0.0.1:' + port + '/web/pho/test.jpg?w=300&force=1', function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    done();
+                    assert.equals(200, response.statusCode);
+                } else {
+                    console.log('response.statusCode:', response.statusCode);
+                    console.log(error);
+                    console.log(response);
+                    done();
+                }
+            });
+        },
+
 
 
 
