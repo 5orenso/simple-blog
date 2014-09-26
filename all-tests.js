10b7c372e75d32552054cb45ad40f316b3afc85b
Øistein Sørensen <sorenso@gmail.com>
2014-09-26 14:00:32 +0200

    Added tests for web and api.

--- all-tests.js
+++ all-tests.js
@@ -2,4 +2,5 @@
 
 require('./test/lib/article-test.js');
 require('./test/lib/logger-test.js');
-//require('./test/app/routes/api-test.js');
+require('./test/app/routes/api-test.js');
+require('./test/app/routes/web-test.js');
