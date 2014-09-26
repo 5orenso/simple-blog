a52f3358659f0aa2156cae2907d7d7cfc3bfe9d0
Øistein Sørensen <sorenso@gmail.com>
2014-09-26 15:07:17 +0200

    Trying to get travis to work.

---
+++ config/config-dist.js
@@ -0,0 +1,17 @@
+module.exports = {
+    version: 1,
+    blog: {
+        title: 'Simple Blog Server',
+        disqus: 'Simple blog server',
+        social: {
+            twitter: '',
+            facebook: '',
+            googleplus: '',
+            pintrest: '',
+            instagram: ''
+        }
+    },
+    app: {
+        port: 8080
+    }
+};
