5e17c6ee9c97a3962c265e8611c74a7de1cde3a9
Øistein Sørensen <sorenso@gmail.com>
2014-09-28 23:33:39 +0200

    Added domain and protocol.

--- config/config-dist.js
+++ config/config-dist.js
@@ -2,6 +2,8 @@ module.exports = {
     version: 1,
     blog: {
         title: 'Simple Blog Server',
+        protocol: 'http',
+        domain: 'www.litt.no',
         disqus: 'Simple blog server',
         social: {
             twitter: '',
