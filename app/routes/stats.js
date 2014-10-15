0171db12f9c5d63a010a814d6553b0fe46b2b6cc
Øistein Sørensen <sorenso@gmail.com>
2014-10-15 22:20:35 +0200

    Added stats route.

---
+++ app/routes/stats.js
@@ -0,0 +1,81 @@
+/*
+ * https://github.com/5orenso
+ *
+ * Copyright (c) 2014 Øistein Sørensen
+ * Licensed under the MIT license.
+ */
+'use strict';
+var express       = require('express'),
+    when          = require('when'),
+    _             = require('underscore'),
+    app_path      = __dirname + '/../../',
+    logger        = require(app_path + 'lib/logger')();
+
+var stats, activeConn, timer_web, timer_api, timer_image, gauge, config;
+var stats_router = express.Router();
+stats_router.set_config = function (conf, opt) {
+    stats_router.config = conf;
+    stats_router.opt = opt;
+    if (opt) {
+        if (opt.hasOwnProperty('workerId')) {
+            logger.set('workerId', opt.workerId);
+        }
+        if (opt.hasOwnProperty('stats')) {
+            stats = opt.stats;
+        }
+        if (opt.hasOwnProperty('activeConn')) {
+            activeConn = opt.activeConn;
+        }
+        if (opt.hasOwnProperty('timer_web')) {
+            timer_web = opt.timer_web;
+        }
+        if (opt.hasOwnProperty('timer_api')) {
+            timer_api = opt.timer_api;
+        }
+        if (opt.hasOwnProperty('timer_image')) {
+            timer_image = opt.timer_image;
+        }
+        if (opt.hasOwnProperty('gauge')) {
+            gauge = opt.gauge;
+        }
+    }
+};
+
+stats_router.use(express.query()); // Parse query_string.
+
+// Main route for html files.
+stats_router.get('/', function(req, res) {
+    var request_pathname = req._parsedUrl.pathname;
+
+    // Start metrics
+    var stopwatch;
+    if (timer_api) { stopwatch = timer_api.start();}
+    if (activeConn) { activeConn.inc(); }
+    if (stats) {
+        stats.meter('requestsPerSecond').mark();
+        stats.meter(request_pathname || '/').mark();
+    }
+    // Stop timer when response is transferred and finish.
+    res.on('finish', function () {
+        if (activeConn) { activeConn.dec(); }
+        if (timer_api) { stopwatch.end(); }
+    });
+    // End metrics
+
+    try {
+        res.header('Content-Type', 'text/plain');
+        res.end(JSON.stringify({
+            stats: stats.toJSON(),
+            activeConn: activeConn.toJSON(),
+            timer_web: timer_web.toJSON(),
+            timer_api: timer_api.toJSON(),
+            timer_image: timer_image.toJSON(),
+            gauge: gauge.toJSON()
+        }, null, 4));
+
+    } catch (err) {
+        res.status(404).send('Page not found: ' + err);
+
+    }
+});
+module.exports = stats_router;
