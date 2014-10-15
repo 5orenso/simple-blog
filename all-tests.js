b6d74ed3f7284d88e799b827801d7efc32c296b8
Øistein Sørensen <sorenso@gmail.com>
2014-10-15 22:16:30 +0200

    Added tests for image and stats.

--- all-tests.js
+++ all-tests.js
@@ -4,3 +4,5 @@ require('./test/lib/article-test.js');
 require('./test/lib/logger-test.js');
 require('./test/app/routes/api-test.js');
 require('./test/app/routes/web-test.js');
+require('./test/app/routes/image-test.js');
+require('./test/app/routes/stats-test.js');
