3f79a4c0853d6c1e9a9db6f3c45df64073623353
Øistein Sørensen <sorenso@gmail.com>
2014-10-24 23:18:26 +0200

    Added an integration test config file.

---
+++ config/config-integration.js
@@ -0,0 +1,28 @@
+module.exports = {
+    version: 1,
+    blog: {
+        title: 'Simple Blog Server',
+        protocol: 'http',
+        domain: 'www.litt.no',
+        disqus: 'Simple blog server',
+        social: {
+            twitter: '',
+            facebook: '',
+            googleplus: '',
+            pintrest: '',
+            instagram: ''
+        },
+        static_files_path: '/Users/sorenso/html/',
+        text_files_path: '/Users/sorenso/text-files/'
+    },
+    app: {
+        port: 8080
+    },
+    adapter: {
+        markdown: {
+            content_path: __dirname + '/../test/content/articles/',
+            photo_path: __dirname + '/../test/content/images/',
+            max_articles: 500,
+        }
+    }
+};
