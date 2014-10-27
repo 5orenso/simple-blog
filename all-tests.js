17a9a51bdc2ccfcdfa03f7fefe2ea2d77cd1244c
Øistein Sørensen <sorenso@gmail.com>
2014-10-27 12:02:28 +0100

    Added more tests.

--- all-tests.js
+++ all-tests.js
@@ -1,7 +1,16 @@
 'use strict';
 
 require('./test/lib/article-test.js');
+require('./test/lib/category-test.js');
 require('./test/lib/logger-test.js');
+
+require('./test/lib/article-util-test.js');
+require('./test/lib/category-util-test.js');
+require('./test/lib/local-util-test.js');
+
+require('./test/lib/adapter/markdown-test.js');
+require('./test/lib/adapter/postgresql-test.js');
+
 require('./test/app/routes/api-test.js');
 require('./test/app/routes/web-test.js');
 require('./test/app/routes/image-test.js');
