963b41606285a5923275fd6cef18afd11a083371
Øistein Sørensen <sorenso@gmail.com>
2014-10-28 10:43:09 +0100

    Added test for all util functions.

--- test/lib/local-util-test.js
+++ test/lib/local-util-test.js
@@ -4,12 +4,38 @@ var buster       = require('buster'),
     assert       = buster.assert,
     refute       = buster.refute,
     when         = require('when'),
-    category     = require('../../lib/local-util')({
+    local_util   = require('../../lib/local-util')({
         logger: {
             log: function () { },
             err: function () { }
         },
     });
+
+var my_array = [
+    { name: 'b', integer: 456 },
+    { name: 'a', integer: 123 },
+    { name: 'c', integer: 789 },
+];
+var my_array_desc_integer = [
+    { name: 'c', integer: 789 },
+    { name: 'b', integer: 456 },
+    { name: 'a', integer: 123 },
+];
+var my_array_asc_integer = [
+    { name: 'a', integer: 123 },
+    { name: 'b', integer: 456 },
+    { name: 'c', integer: 789 },
+];
+var my_array_desc_name = [
+    { name: 'c', integer: 789 },
+    { name: 'b', integer: 456 },
+    { name: 'a', integer: 123 },
+];
+var my_array_asc_name = [
+    { name: 'a', integer: 123 },
+    { name: 'b', integer: 456 },
+    { name: 'c', integer: 789 },
+];
 
 
 buster.testCase('lib/local-util', {
@@ -19,11 +45,28 @@ buster.testCase('lib/local-util', {
     },
     'Test local-util:': {
         'sortByKey': function () {
-            assert(true);
+            var result = local_util.sortByKey(my_array, 'integer');
+            assert.equals(result, my_array_desc_integer);
+
+            result = local_util.sortByKey(my_array, 'integer').reverse();
+            assert.equals(result, my_array_asc_integer);
+
+            result = local_util.sortByKey(my_array, 'name');
+            assert.equals(result, my_array_desc_name);
+
+            result = local_util.sortByKey(my_array, 'name').reverse();
+            assert.equals(result, my_array_asc_name);
         },
 
         'quote_url': function () {
+            var result = local_util.quote_url('this is an url/with special chars & other ?');
+            assert.equals(result, 'this%20is%20an%20url/with%20special%20chars%20&amp;%20other%20?');
             assert(true);
+        },
+
+        'quote_url w/no input': function () {
+            var result = local_util.quote_url();
+            assert.equals(result, '');
         },
 
     }
