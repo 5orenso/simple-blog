becd9f5ac376d46ca0070b1f1802df261e8ef117
Øistein Sørensen <sorenso@gmail.com>
2014-11-04 20:01:06 +0100

    Removed logging. Added metrics.

--- app/routes/api.js
+++ app/routes/api.js
@@ -22,48 +22,33 @@ api_router.set_config = function (conf, opt) {
         if (opt.hasOwnProperty('workerId')) {
             logger.set('workerId', opt.workerId);
         }
-        if (opt.hasOwnProperty('stats')) {
-            stats = opt.stats;
-        }
-        if (opt.hasOwnProperty('activeConn')) {
-            activeConn = opt.activeConn;
-        }
-        if (opt.hasOwnProperty('timer')) {
-            timer = opt.timer;
-        }
     }
 };
 // middleware to use for all requests
 api_router.use(function(req, res, next) {
     // do logging
-    logger.log(
-        req.method,
-            '/api' + req.url,
-        req.get('Content-type'),
-        req.get('User-agent')
-    );
+//    logger.log(
+//        req.method,
+//            '/api' + req.url,
+//        req.get('Content-type'),
+//        req.get('User-agent')
+//    );
     next(); // make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 api_router.get('/', function(req, res) {
+    var lu    = require(app_path + 'lib/local-util')({config: api_router.config});
     var request_pathname = req._parsedUrl.pathname;
 
-    // Start metrics
-    var stopwatch;
-    if (timer) { stopwatch = timer.start();}
-    if (activeConn) { activeConn.inc(); }
-    if (stats) {
-        stats.meter('requestsPerSecond').mark();
-        stats.meter('/api' + request_pathname).mark();
-    }
+
+    lu.timers_reset();
+    lu.timer('routes/api->request');
     // Stop timer when response is transferred and finish.
     res.on('finish', function () {
-        if (activeConn) { activeConn.dec(); }
-        if (timer) { stopwatch.end(); }
+        lu.timer('routes/api->request');
+        lu.send_udp({ timers: lu.timers_get() });
     });
-    // End metrics
-
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
