d4b38a0f4cd4b557c4963cf3afaa39a9f870a8f4
Øistein Sørensen <sorenso@gmail.com>
2014-12-29 18:40:59 +0100

    Default setup inside config.

--- config/config-dist.js
+++ config/config-dist.js
@@ -16,7 +16,7 @@ module.exports = {
         static_files_path: '/Users/sorenso/html/',
         text_files_path: '/Users/sorenso/text-files/',
         top_image: false, // Don't use image[0] as top image on site. Use top_image instead.
-        simple_header: true, // Use simple header instead of top panorama
+        simple_header: false, // Use simple header instead of top panorama
         //google_analytics: 'UA-xxxxxxxx-1',
         //google_tag_manager: 'GTM-xxxxxx',
         author: {
