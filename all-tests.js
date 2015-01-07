01fb67dd4dbac72ec38de98894243f86e051344e
Øistein Sørensen <sorenso@gmail.com>
2015-01-07 11:29:41 +0100

    Added search tests.

--- all-tests.js
+++ all-tests.js
@@ -3,6 +3,7 @@
 require('./test/lib/article-test.js');
 require('./test/lib/category-test.js');
 require('./test/lib/logger-test.js');
+require('./test/lib/search-test.js');
 
 require('./test/lib/article-util-test.js');
 require('./test/lib/category-util-test.js');
@@ -10,8 +11,10 @@ require('./test/lib/local-util-test.js');
 
 require('./test/lib/adapter/markdown-test.js');
 require('./test/lib/adapter/postgresql-test.js');
+require('./test/lib/adapter/elasticsearch-test.js');
 
 require('./test/app/routes/api-test.js');
 require('./test/app/routes/web-test.js');
 require('./test/app/routes/image-test.js');
 require('./test/app/routes/stats-test.js');
+require('./test/app/routes/search-test.js');
