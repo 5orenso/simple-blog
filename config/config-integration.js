70f586fb663d2e9db7a8dc83b79278cf70f06872
Øistein Sørensen <sorenso@gmail.com>
2015-01-07 16:42:49 +0100

    Added mock_services as config.

--- config/config-integration.js
+++ config/config-integration.js
@@ -19,7 +19,7 @@ module.exports = {
 
         rewrites: [
             { url: '/photoalbum/view.*', target: '/web/?q=', code: 302, use_url: true, regex: /^\/photoalbum\/view\/(.+?)$/, regex_result: "$1" },
-            { url: '/tools/.*', target: '/', code: 302 }
+            { url: '/tools/.*', target: '/web/', code: 302 }
         ]
     },
     app: {
@@ -31,6 +31,70 @@ module.exports = {
     },
 
     adapter: {
+        mock_services: {
+            //adapter: {
+            //    service: {
+            //        functions
+            //    }
+            //}
+            elasticsearch: {
+                elasticsearch: {
+                    Client: function () {
+                        var when   = require('when');
+                        var article = {
+                            tag_values: { toc: '', fact: '', artlist: '' },
+                            published: '2014-01-01',
+                            title: 'Simple blog 2',
+                            img: [ 'simple-blog.jpg' ],
+                            body: 'This is content number 2.',
+                            file: 'index',
+                            filename: 'my-path-to-the-files/content/articles/simple-blog/simple-blog.md',
+                            base_href: '/simple-blog/'
+                        };
+                        return {
+                            ping: function (opt, callback) {
+                                callback(null, {status: 'ok'});
+                            },
+                            search: function (query) {
+                                return when.promise(function (resolve, reject) {
+                                    if (query.body.query.match === 'one-hit') {
+                                        resolve({
+                                            hits: {
+                                                hits: [{
+                                                    _source: article
+                                                }]
+                                            },
+                                            query: query
+                                        });
+                                    } else if (query.body.query.match === 'no-hit') {
+                                        resolve({
+                                            hits: {
+                                                hits: []
+                                            },
+                                            query: query
+                                        });
+                                    } else if (query.body.query.match === 'two-hit') {
+                                        resolve({
+                                            hits: {
+                                                hits: [{
+                                                    _source: article
+                                                }, {
+                                                    _source: article
+                                                }]
+                                            },
+                                            query: query
+                                        });
+                                    }
+                                });
+                            },
+                            index: function (obj, callback) {
+                                callback(null, {status: 'ok'});
+                            }
+                        };
+                    }
+                }
+            }
+        },
         current: 'markdown',
         markdown: {
             content_path: __dirname + '/../test/content/articles/',
@@ -45,6 +109,12 @@ module.exports = {
             database: 'test',
             article_table: 'article',
             category_table: 'category'
+        },
+        elasticsearch: {
+            server: '127.0.0.1',
+            port: 9200,
+            index: 'twitter',
+            type: 'tweet'
         }
     }
 };
