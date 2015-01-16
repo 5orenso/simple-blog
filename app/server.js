e33f9a64f9926df87b990405eee881de059e20b0
Øistein Sørensen <sorenso@gmail.com>
2015-01-16 12:57:14 +0100

    Removed cluster module due to performance issues.

--- app/server.js
+++ app/server.js
@@ -6,84 +6,59 @@
  */
 'use strict';
 
-var cluster = require('cluster');
-var worker;
+var when = require('when'),
+    _ = require('underscore'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    commander = require('commander'),
+    app_path = __dirname + '/../',
+    logger = require(app_path + 'lib/logger')({
+        workerId: 1 //cluster.worker.id
+    }),
+    local_util = require(app_path + 'lib/local-util')();
 
-if (cluster.isMaster) {
-    // Code to run if we're in the master process
-    // Count the machine's CPUs
-    var cpuCount = require('os').cpus().length;
+var app = express();
 
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
-        worker = cluster.fork();
+commander
+    .option('-c, --config <file>', 'configuration file path', './config/config.js')
+    .parse(process.argv);
+var config = require(commander.config);
+if (config) {
+    if (_.isObject(config) && _.isObject(config.log)) {
+        logger.set('log', config.log);
     }
-
-} else {
-    // Code to run if we're in a worker process
-    var when = require('when'),
-        _ = require('underscore'),
-        express = require('express'),
-        bodyParser = require('body-parser'),
-        path = require('path'),
-        commander = require('commander'),
-        app_path = __dirname + '/../',
-        logger = require(app_path + 'lib/logger')({
-            workerId: cluster.worker.id
-        }),
-        local_util    = require(app_path + 'lib/local-util')();
-
-    var app = express();
-
-    commander
-        .option('-c, --config <file>', 'configuration file path', './config/config.js')
-        .parse(process.argv);
-    var config = require(commander.config);
-    if (config) {
-        if (_.isObject(config) && _.isObject(config.log)) {
-            logger.set('log', config.log);
-        }
-    }
-
-    app.use(bodyParser.json());
-
-    // Include route handlers ------------------------
-    var api_router = require('./routes/api');
-    api_router.set_config(config, {
-        workerId: cluster.worker.id,
-    });
-    var web_router = require('./routes/web');
-    web_router.set_config(config, {
-        workerId: cluster.worker.id,
-    });
-    var image_router  = require('./routes/image');
-    image_router.set_config(config, {
-        workerId: cluster.worker.id,
-    });
-    var search_router  = require('./routes/search');
-    search_router.set_config(config, {
-        workerId: cluster.worker.id,
-    });
-
-    // Register routes -------------------------------
-    app.use('/api', api_router);
-    app.use('/static', local_util.set_cache_headers);
-    app.use('/static', express.static(config.blog.static_files_path));
-    app.use('/.well-known/', express.static(config.blog.text_files_path));
-    app.use('/pho/', image_router);
-    app.use('/search/', search_router);
-    app.use('/', web_router);
-
-    // Start the server ------------------------------
-    var server = app.listen(config.app.port);
-    logger.log('Something happens on port ' + config.app.port);
 }
 
-// Listen for dying workers
-cluster.on('exit', function (worker) {
-    // Replace the dead worker,
-    // we're not sentimental
-    console.log('Worker ' + worker.id + ' died :(');
-    cluster.fork();
+app.use(bodyParser.json());
 
+// Include route handlers ------------------------
+var api_router = require('./routes/api');
+api_router.set_config(config, {
+    workerId: 1 //cluster.worker.id,
 });
+var web_router = require('./routes/web');
+web_router.set_config(config, {
+    workerId: 1 //cluster.worker.id,
+});
+var image_router = require('./routes/image');
+image_router.set_config(config, {
+    workerId: 1 //cluster.worker.id,
+});
+var search_router = require('./routes/search');
+search_router.set_config(config, {
+    workerId: 1 //cluster.worker.id,
+});
+
+// Register routes -------------------------------
+app.use('/api', api_router);
+app.use('/static', local_util.set_cache_headers);
+app.use('/static', express.static(config.blog.static_files_path));
+app.use('/.well-known/', express.static(config.blog.text_files_path));
+app.use('/pho/', image_router);
+app.use('/search/', search_router);
+app.use('/', web_router);
+
+// Start the server ------------------------------
+var server = app.listen(config.app.port);
+logger.log('Something happens on port ' + config.app.port);
