86ac2938c1ecd5464134c2e096d9c6a58a0e0894
Øistein Sørensen <sorenso@gmail.com>
2015-09-05 21:38:17 +0200

    Added tests for all code lines.

--- test/lib/metrics-test.js
+++ test/lib/metrics-test.js
@@ -1,9 +1,22 @@
 'use strict';
 
-var buster  = require('buster'),
-    assert  = buster.assert,
-    Metrics = require('../../lib/metrics'),
-    metrics  = new Metrics();
+var buster    = require('buster'),
+    assert    = buster.assert,
+    StatsD    = require('node-dogstatsd').StatsD,
+    //dogstatsd = new StatsD(),
+    sinon     = require('sinon'),
+    Metrics   = require('../../lib/metrics'),
+    metrics   = new Metrics({
+        useDataDog: true
+    });
+
+sinon.stub(StatsD.prototype, 'timing', function (metricName, timeInMillisec) {
+    console.log('stub called with: ', metricName, timeInMillisec);
+});
+//sinon.spy(dogstatsd, "timing");
+sinon.stub(StatsD.prototype, 'increment', function (metricName) {
+    console.log('stub called with: ', metricName);
+});
 
 buster.testCase('lib/metrics', {
     setUp: function () {
@@ -28,39 +41,22 @@ buster.testCase('lib/metrics', {
             syncFunc = metrics.hook(syncFunc, 'syncFunc');  // jshint ignore:line
             var result = syncFunc('main');
             var hookMeta = metrics.hookMeta();
-            console.log('metrics-test.result = ', result, hookMeta);
+            //console.log('metrics-test.result = ', result, hookMeta);
             assert.equals(result, 'main');
             assert.isNumber(hookMeta.pre);
             assert.isNumber(hookMeta.post);
             assert.near(hookMeta.post, 250, 250);
             assert.greater(hookMeta.pre, 1441000000000);
+            console.log(sinon.assert.callCount(StatsD.prototype.timing, 1));
+            //console.log(stub.callCount());
+            //assert(dogstatsd.timing.calledWithMatch('syncFunc', ));
+            //assert(dogstatsd.timing.called());
+        },
+
+        'call metrics increment': function () {
+            assert(metrics.increment('my.metric'));
+            console.log(sinon.assert.callCount(StatsD.prototype.increment, 1));
         }
-        //
-        //'add hooks to a async function': function (done) {
-        //    function asyncFunc(input, callback) {
-        //        setTimeout(function () {
-        //            callback(input);
-        //        }, 1000);
-        //    }
-        //    asyncFunc = hook.add(asyncFunc, function () { // jshint ignore:line
-        //        return 'cb-pre';
-        //    }, function () {
-        //        return 'cb-post';
-        //    });
-        //    when(when.promise(function(resolve) {
-        //        asyncFunc('cb-main', function (result) {
-        //            resolve(result);
-        //        });
-        //    })).
-        //        done(function (result) {
-        //            var hookMeta = hook.meta();
-        //            //console.log(' = hook-test.result = ', result, hookMeta);
-        //            assert.equals(result, 'cb-main');
-        //            assert.equals(hookMeta.pre, 'cb-pre');
-        //            assert.equals(hookMeta.post, 'cb-post');
-        //            done();
-        //        });
-        //}
 
     }
 });
