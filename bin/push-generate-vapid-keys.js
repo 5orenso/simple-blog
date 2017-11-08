c665be897758a7fb072b5557cd75cae139050313
Oistein Sorensen <sorenso@gmail.com>
2017-11-08 12:48:24 +0100

    Made output a bit nicer for copy-paste

--- bin/push-generate-vapid-keys.js
+++ bin/push-generate-vapid-keys.js
@@ -3,4 +3,5 @@ const webpush = require('web-push');
 const vapidKeys = webpush.generateVAPIDKeys();
 
 // Prints 2 URL Safe Base64 Encoded Strings
-console.log(vapidKeys.publicKey, vapidKeys.privateKey);
+console.log('publicKey:', `'${vapidKeys.publicKey}',`);
+console.log('privateKey:', `'${vapidKeys.privateKey}',`);
