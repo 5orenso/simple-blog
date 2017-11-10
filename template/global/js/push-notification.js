1bb167b56526edc2fbde25030b869aa7e508b839
Oistein Sorensen <sorenso@gmail.com>
2017-11-10 10:43:59 +0100

    Fixed linting.

--- template/global/js/push-notification.js
+++ template/global/js/push-notification.js
@@ -1,26 +1,6 @@
-function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/')
-    ;
-    const rawData = window.atob(base64);
-    return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
-}
-
 function registerServiceWorker() {
     return navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-            console.log('[push-notifications.js]  Service worker successfully registered.');
-            registration.onupdatefound = () => {
-                // If updatefound is fired, it means that there's a new service worker being installed.
-                // You can listen for changes to the installing service worker's state
-                // via installingWorker.onstatechange
-                var installingWorker = registration.installing;
-                console.log('[push-notifications.js]  A new service worker is being installed:', installingWorker);
-            };
-            return registration;
-        })
+        .then(registration => registration)
         .catch((err) => {
             throw new Error(`Unable to register service worker: ${err}`);
         });
@@ -47,9 +27,11 @@ function sendErrorToBackend(error) {
     return fetch('/push-error', {
         method: 'post',
         headers: {
-            'Content-type': 'application/json'
+            'Content-type': 'application/json',
         },
         body: JSON.stringify({
+            // eslint-disable-next-line
+            email: SESSION_EMAIL,
             error: error.name,
             errorMessage: error.message,
         }),
@@ -71,24 +53,26 @@ function sendErrorToBackend(error) {
 function sendSubscriptionToBackEnd(subscription) {
     // Retrieve the user's public key.
     if (typeof subscription !== 'object') {
-        return Promise.reject('sendSubscriptionToBackEnd: No subscripton found');
+        return Promise.reject(new Error('sendSubscriptionToBackEnd: No subscripton found'));
     }
-    var rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
-    var key = rawKey ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : '';
-    var rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : '';
-    authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : '';
-    endpoint = subscription.endpoint;
+    const rawKey = subscription.getKey ? subscription.getKey('p256dh') : '';
+    const key = rawKey ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawKey))) : '';
+    const rawAuthSecret = subscription.getKey ? subscription.getKey('auth') : '';
+    const authSecret = rawAuthSecret ? btoa(String.fromCharCode.apply(null, new Uint8Array(rawAuthSecret))) : '';
+    const endpoint = subscription.endpoint;
 
     // Send the subscription details to the server using the Fetch API.
     return fetch('/push-register', {
         method: 'post',
         headers: {
-            'Content-type': 'application/json'
+            'Content-type': 'application/json',
         },
         body: JSON.stringify({
-            endpoint: subscription.endpoint,
-            key: key,
-            authSecret: authSecret,
+            // eslint-disable-next-line
+            email: SESSION_EMAIL,
+            endpoint,
+            key,
+            authSecret,
         }),
     })
         .then((response) => {
@@ -107,62 +91,52 @@ function sendSubscriptionToBackEnd(subscription) {
 
 function subscribeUserToPush() {
     return new Promise((resolve, reject) => {
-        return navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
-            console.log('[push-notifications.js]  Service worker is ready.');
-            return serviceWorkerRegistration.pushManager.getSubscription()
-                .then((subscription) => {
-                    // If a subscription was found, return it.
-                    if (subscription) {
-                        console.log('[push-notifications.js]  pushManager.getSubscription.subscription:', subscription);
-                        return resolve(subscription);
-                    }
-
-                    console.log('[push-notifications.js]  pushManager.getSubscription: No subscription found. Asking for permission.');
-                    return askPermission()
-                        .then(() => {
-                            // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
-                            // send browser push notifications that don't have a visible effect for the user).
-                            const subscribeOptions = {
-                                userVisibleOnly: true,
-                                // applicationServerKey: urlBase64ToUint8Array(
-                                //     '',
-                                // ),
-                            };
-                            console.log(`[push-notifications.js]  pushManager.subscribe(${JSON.stringify(subscribeOptions)})`);
-                            return serviceWorkerRegistration.pushManager.subscribe(subscribeOptions);
-                        })
-                        .then((pushSubscription) => {
-                            console.log('[push-notifications.js]  pushManager.subscribe.pushSubscription:', pushSubscription);
-                            // console.log('[push-notifications.js]  pushSubscription.subscriptionId:', pushSubscription.subscriptionId);
-                            // The push subscription details needed by the application
-                            // server are now available, and can be sent to it using,
-                            // for example, an XMLHttpRequest.
-                            return resolve(pushSubscription);
-                        })
-                        .catch((error) => {
-                            // During development it often helps to log errors to the
-                            // console. In a production environment it might make sense to
-                            // also report information about errors back to the
-                            // application server.
-                            // console.log('[push-notifications.js]  pushManager.subscribe.error: ', error);
-                            reject(error);
-                        });
-                });
-        });
+        let serviceWorkerRegistration;
+        return navigator.serviceWorker.ready
+            .then((swReg) => {
+                serviceWorkerRegistration = swReg;
+                return serviceWorkerRegistration.pushManager.getSubscription();
+            })
+            .then((subscription) => {
+                if (subscription) {
+                    console.log('[push]  pushManager.getSubscription.subscription:', subscription);
+                    return resolve(subscription);
+                }
+                console.log('[push]  pushManager.getSubscription: No subscription found. ' +
+                    'Asking for permission.');
+                return askPermission()
+                    .then(() => {
+                        const subscribeOptions = {
+                            userVisibleOnly: true,
+                        };
+                        console.log(`[push] pushManager.subscribe(${JSON.stringify(subscribeOptions)})`);
+                        return serviceWorkerRegistration.pushManager.subscribe(subscribeOptions);
+                    })
+                    .then((pushSubscription) => {
+                        console.log('[push]  pushManager.subscribe.pushSubscription:', pushSubscription);
+                        return resolve(pushSubscription);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            });
     });
 }
 
 if ('serviceWorker' in navigator) {
     registerServiceWorker()
-        .then((registration) => subscribeUserToPush(registration))
-        .then((subscription) => sendSubscriptionToBackEnd(subscription))
+        .then(registration => subscribeUserToPush(registration))
+        .then(subscription => sendSubscriptionToBackEnd(subscription))
         .then((result) => {
+            // All ok. Service worker is registered and sent to the server.
             console.log('[push-notifications.js]  Response from server:', result);
         })
         .catch((error) => {
+            // Something went wrong. Trying to send error to the server.
             console.log('[push-notifications.js]  Ok, so something went wrong:', error);
             sendErrorToBackend(error)
                 .then((response) => {
+                    // Error sent to the server.
                     console.log(`[push-notifications.js]  sendErrorToBackend.response: ${JSON.stringify(response)}`);
                 })
                 .catch((err) => {
@@ -170,5 +144,6 @@ if ('serviceWorker' in navigator) {
                 });
         });
 } else {
+    // Server worker is not supported.
     console.log('[push-notifications.js]  Service workers are not supported.');
 }
