a084cdb1c1a35afa66a70248d26b6ab51859b571
Oistein Sorensen <sorenso@gmail.com>
2019-08-12 10:26:58 +0200

    Fixed search and menu.

--- app/routes/v2/get-search.js
+++ app/routes/v2/get-search.js
@@ -4,22 +4,22 @@
  * Copyright (c) 2019 Øistein Sørensen
  * Licensed under the MIT license.
  */
+
 'use strict';
 
-const { routeName, routePath, run, webUtil, utilHtml } = require('../../middleware/init')({ __filename, __dirname });
+const { routeName, routePath, run, webUtil } = require('../../middleware/init')({ __filename, __dirname });
 
 const Article = require('../../../lib/class/article');
 const Category = require('../../../lib/class/category');
 
 module.exports = async (req, res) => {
-    const { hrstart, runId }  = run(req);
+    const { hrstart, runId } = run(req);
 
     const art = new Article();
     const cat = new Category();
+    const catMenu = new Category();
 
-    let query = {
-        status: 2,
-    };
+    const query = { status: 2 };
     if (req.params.category) {
         query.category = req.params.category;
     }
@@ -27,13 +27,16 @@ module.exports = async (req, res) => {
     const limit = parseInt(req.query.limit || 10, 10);
     const skip = parseInt((page - 1) * limit || 0, 10);
 
+    const contentCatlist = await cat.find({ type: { $nin: [2, 3] } });
+    query.categoryId = { $in: contentCatlist.map(c => c.id) };
+
     // const article = await art.findOne(query);
     const { list, total } = await art.search(req.query.q, {}, { limit, skip, query });
     const artlist = list;
     const artlistTotal = total;
 
     const category = await cat.findOne({ title: req.params.category });
-    const catlist = await cat.find();
+    const catlist = await catMenu.find({ menu: 1 });
 
     const template = (req.params.id || req.params.filename) ? '/bootstrap4/blog_v2.html' : '/bootstrap4/index_v2.html';
 
