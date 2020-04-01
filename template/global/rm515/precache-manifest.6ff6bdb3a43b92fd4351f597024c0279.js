2fa16828a5f93fa17488285907cb552296fb91d8
Oistein Sorensen <sorenso@gmail.com>
2020-04-01 14:54:37 +0200

    Added test map

---
+++ template/global/rm515/precache-manifest.6ff6bdb3a43b92fd4351f597024c0279.js
@@ -0,0 +1,93 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/2273e3d8ad9264b7daa5bdbf8e6b47f8.png"
+  },
+  {
+    "url": "/401d815dc206b8dc1b17cd0e37695975.png"
+  },
+  {
+    "url": "/44a526eed258222515aa21eaffd14a96.png"
+  },
+  {
+    "url": "/4f0283c6ce28e888000e978e537a6a56.png"
+  },
+  {
+    "url": "/a6137456ed160d7606981aa57c559898.png"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e36f03d2c53d3cb5a8f4",
+    "url": "/bundle.7e56a.css"
+  },
+  {
+    "revision": "e36f03d2c53d3cb5a8f4",
+    "url": "/bundle.e36f0.js"
+  },
+  {
+    "revision": "0490387de48df17d76abce05a8b85269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09446198657efe85c470",
+    "url": "/polyfills.09446.js"
+  },
+  {
+    "revision": "54553782ffd26d8b0ff2",
+    "url": "/route-home.chunk.54553.js"
+  },
+  {
+    "revision": "54553782ffd26d8b0ff2",
+    "url": "/route-home.chunk.bf30f.css"
+  },
+  {
+    "revision": "1dfc45879baab70f206e",
+    "url": "/route-profile.chunk.1dfc4.js"
+  },
+  {
+    "revision": "1dfc45879baab70f206e",
+    "url": "/route-profile.chunk.62c75.css"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "3146d188b80ed3f50d0b27c905dc7c92",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  },
+  {
+    "revision": "24c791a5044145a13c89",
+    "url": "/vendors~route-home.chunk.24c79.js"
+  },
+  {
+    "revision": "24c791a5044145a13c89",
+    "url": "/vendors~route-home.chunk.26607.css"
+  }
+]);
---
+++ template/global/rm515/precache-manifest.79813c312cd999bb8d97c0a916dcdc9b.esm.js
@@ -0,0 +1,81 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/2273e3d8ad9264b7daa5bdbf8e6b47f8.png"
+  },
+  {
+    "url": "/401d815dc206b8dc1b17cd0e37695975.png"
+  },
+  {
+    "url": "/44a526eed258222515aa21eaffd14a96.png"
+  },
+  {
+    "url": "/4f0283c6ce28e888000e978e537a6a56.png"
+  },
+  {
+    "url": "/a6137456ed160d7606981aa57c559898.png"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e36f03d2c53d3cb5a8f4",
+    "url": "/bundle.362c5.esm.js"
+  },
+  {
+    "revision": "e36f03d2c53d3cb5a8f4",
+    "url": "/bundle.7e56a.css"
+  },
+  {
+    "revision": "0490387de48df17d76abce05a8b85269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09446198657efe85c470",
+    "url": "/polyfills.a34c8.esm.js"
+  },
+  {
+    "revision": "54553782ffd26d8b0ff2",
+    "url": "/route-home.chunk.7599c.esm.js"
+  },
+  {
+    "revision": "54553782ffd26d8b0ff2",
+    "url": "/route-home.chunk.bf30f.css"
+  },
+  {
+    "revision": "1dfc45879baab70f206e",
+    "url": "/route-profile.chunk.4dd45.esm.js"
+  },
+  {
+    "revision": "1dfc45879baab70f206e",
+    "url": "/route-profile.chunk.62c75.css"
+  },
+  {
+    "revision": "24c791a5044145a13c89",
+    "url": "/vendors~route-home.chunk.26607.css"
+  },
+  {
+    "revision": "24c791a5044145a13c89",
+    "url": "/vendors~route-home.chunk.c1848.esm.js"
+  }
+]);
