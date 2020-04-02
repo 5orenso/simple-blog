73a193a4949bc411c1f5b94b6d822c4b015d01ce
Oistein Sorensen <sorenso@gmail.com>
2020-04-02 16:36:53 +0200

    New map release

---
+++ template/global/rm515/precache-manifest.822eb677f0985e1eaf256043201c00ea.esm.js
@@ -0,0 +1,73 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/global/rm515/2273e3d8ad9264b7daa5bdbf8e6b47f8.png"
+  },
+  {
+    "url": "/global/rm515/401d815dc206b8dc1b17cd0e37695975.png"
+  },
+  {
+    "url": "/global/rm515/44a526eed258222515aa21eaffd14a96.png"
+  },
+  {
+    "url": "/global/rm515/4f0283c6ce28e888000e978e537a6a56.png"
+  },
+  {
+    "url": "/global/rm515/a6137456ed160d7606981aa57c559898.png"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/global/rm515/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/global/rm515/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/global/rm515/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/global/rm515/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/global/rm515/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/global/rm515/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "7c31ffa2033c2873523e",
+    "url": "/global/rm515/bundle.10a45.esm.js"
+  },
+  {
+    "revision": "7c31ffa2033c2873523e",
+    "url": "/global/rm515/bundle.7e56a.css"
+  },
+  {
+    "revision": "bc546f19f106f952997b4096d0e24073",
+    "url": "/global/rm515/index.html"
+  },
+  {
+    "revision": "82d8ada2eea1d20abf6b",
+    "url": "/global/rm515/polyfills.fcf74.esm.js"
+  },
+  {
+    "revision": "2ad02ad3be380ec1cd8a",
+    "url": "/global/rm515/route-home.chunk.475da.esm.js"
+  },
+  {
+    "revision": "2ad02ad3be380ec1cd8a",
+    "url": "/global/rm515/route-home.chunk.98de8.css"
+  },
+  {
+    "revision": "1dfc45879baab70f206e",
+    "url": "/global/rm515/route-profile.chunk.4dd45.esm.js"
+  },
+  {
+    "revision": "1dfc45879baab70f206e",
+    "url": "/global/rm515/route-profile.chunk.62c75.css"
+  }
+]);
