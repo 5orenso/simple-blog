788bb98d859fe5d1831c3b01f3d5a06a9dd551d6
Oistein Sorensen <sorenso@gmail.com>
2020-05-21 12:58:59 +0200

    Added new version of the map.

---
+++ template/global/rm515/precache-manifest.46118fabd8940ef33334c792dfaf41e5.esm.js
@@ -0,0 +1,109 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/global/rm515/2273e3d8ad9264b7daa5bdbf8e6b47f8.png"
+  },
+  {
+    "url": "/global/rm515/401d815dc206b8dc1b17cd0e37695975.png"
+  },
+  {
+    "url": "/global/rm515/44a526eed258222515aa21eaffd14a96.png"
+  },
+  {
+    "url": "/global/rm515/4f0283c6ce28e888000e978e537a6a56.png"
+  },
+  {
+    "url": "/global/rm515/a6137456ed160d7606981aa57c559898.png"
+  },
+  {
+    "revision": "77001d2b507b945d29259f7a85b64266",
+    "url": "/global/rm515/assets/515-overlay-v2.png"
+  },
+  {
+    "revision": "4f90a63a8b033f08589d06f74991611e",
+    "url": "/global/rm515/assets/515-overlay.png"
+  },
+  {
+    "revision": "dc080ec74e61232ea06288d7044fdbad",
+    "url": "/global/rm515/assets/flag.png"
+  },
+  {
+    "revision": "58ac043178163dfcdd6893347a290f40",
+    "url": "/global/rm515/assets/frame.png"
+  },
+  {
+    "revision": "7f3c4a6e749d26e2c2400a33cddb2826",
+    "url": "/global/rm515/assets/heart-draw.png"
+  },
+  {
+    "revision": "bfe174e2d12736cd23a2f4b05f591b88",
+    "url": "/global/rm515/assets/heart.png"
+  },
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/global/rm515/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/global/rm515/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/global/rm515/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/global/rm515/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/global/rm515/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/global/rm515/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "41eb3f51f30aa3948d9855a125208bdf",
+    "url": "/global/rm515/assets/logo.png"
+  },
+  {
+    "revision": "47f342be5c545f42323d6a55686b47ec",
+    "url": "/global/rm515/assets/wave.png"
+  },
+  {
+    "revision": "f4d31fa5e24fd38f4673",
+    "url": "/global/rm515/bundle.c7026.esm.js"
+  },
+  {
+    "revision": "f4d31fa5e24fd38f4673",
+    "url": "/global/rm515/bundle.ea887.css"
+  },
+  {
+    "revision": "4aff992cc49d4bf669044e1a6cca64f8",
+    "url": "/global/rm515/index.html"
+  },
+  {
+    "revision": "82cdd45c0c68216ee45b",
+    "url": "/global/rm515/polyfills.d247a.esm.js"
+  },
+  {
+    "revision": "d8e4f02b42ca87fa9a03",
+    "url": "/global/rm515/route-home.chunk.04c6f.esm.js"
+  },
+  {
+    "revision": "d8e4f02b42ca87fa9a03",
+    "url": "/global/rm515/route-home.chunk.f30f5.css"
+  },
+  {
+    "revision": "25f9166e8565232d74eb",
+    "url": "/global/rm515/route-home~route-profile.chunk.89e7e.esm.js"
+  },
+  {
+    "revision": "8d78738d4d27bce8048e",
+    "url": "/global/rm515/route-profile.chunk.62c75.css"
+  },
+  {
+    "revision": "8d78738d4d27bce8048e",
+    "url": "/global/rm515/route-profile.chunk.f892b.esm.js"
+  }
+]);
