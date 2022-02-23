baa6197724157d20d9649ab23a304fa586f62e0a
Oistein Sorensen <sorenso@gmail.com>
2022-02-23 15:49:22 +0100

    Template adjustments

--- preact/simple-blog-gallery/src/component.js
+++ preact/simple-blog-gallery/src/component.js
@@ -125,9 +125,9 @@ export default function App(props) {
             {/* {JSON.stringify(images, null, 4)} */}
             {/* {JSON.stringify(article)} */}
 
-            <div class={`w-100 no-scrollbar ${article['gallery-wrapper-class']}`}>
+            <div class={`w-100 ${article['gallery-wrapper-class']}`}>
                 <div
-                    class={`d-flex flex-row flex-nowrap ${article['gallery-wrapper-inner-class'] ? article['gallery-wrapper-inner-class'] : 'border-top border-bottom'}`}
+                    class={`d-flex flex-row flex-nowrap no-scrollbar ${article['gallery-wrapper-inner-class'] ? article['gallery-wrapper-inner-class'] : 'border-top border-bottom'}`}
                     style='overflow: auto; scroll-snap-type: x mandatory;'
                     onScroll={scrollImages}
                     ref={imageScrollerRef}
