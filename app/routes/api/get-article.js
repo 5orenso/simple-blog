583f7e0e5fb94a9dce2676de80edff96e60cc008
Oistein Sorensen <sorenso@gmail.com>
2022-03-02 13:38:49 +0100

    Template adjustments

--- app/routes/api/get-article.js
+++ app/routes/api/get-article.js
@@ -142,10 +142,10 @@ module.exports = async (req, res) => {
     const skip = parseInt(req.query.offset || 0, 10);
 
     if (req.query.category) {
-        query.category = req.query.category;
+        query.category = parseInt(req.query.category, 10);
     }
     if (req.query.categoryIn && req.query.categoryIn.length > 0) {
-        query.categoryIn = { $in: req.query.categoryIn.split(',') };
+        query.categoryIn = { $in: req.query.categoryIn.split(',').map(c => parseInt(c, 10)) };
     }
     if (req.query.status) {
         query.status = req.query.status;
