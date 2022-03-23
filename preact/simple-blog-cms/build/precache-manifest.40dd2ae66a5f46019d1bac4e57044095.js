80f5f35c323db34f3bd1c1080a6384171ca5863c
Oistein Sorensen <sorenso@gmail.com>
2022-03-23 12:48:42 +0100

    new build

---
+++ preact/simple-blog-cms/build/precache-manifest.40dd2ae66a5f46019d1bac4e57044095.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/preact/simple-blog-cms/assets/icon.png"
+  },
+  {
+    "revision": "669f2c3bb9c6b0e71ae2",
+    "url": "/preact/simple-blog-cms/bundle.js"
+  },
+  {
+    "revision": "94eae66bebbd6bbfe48a669f245048ac",
+    "url": "/preact/simple-blog-cms/favicon.ico"
+  },
+  {
+    "revision": "c61f84cc06b36189cbf0f8f28fcaebca",
+    "url": "/preact/simple-blog-cms/index.html"
+  }
+]);
