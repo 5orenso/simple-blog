f3d10db633224142510675daf58cd4837afb87db
Oistein Sorensen <sorenso@gmail.com>
2023-01-31 14:02:18 +0100

    Added new booking route

--- app/routes/api/bookings/index.js
+++ app/routes/api/bookings/index.js
@@ -5,6 +5,7 @@ const { utilExpress: webUtil } = require('node-simple-utilities');
 const wrap = require('../../../middleware/wrap');
 
 // Routes:
+router.get('/sheet/:sheetid', wrap(require('./get-sheet')));
 router.get('/:sheetid', wrap(require('./get-booking')));
 router.post('/:sheetid', wrap(require('./post-booking')));
 router.post('/log/:sheetid', wrap(require('./post-log-booking')));
