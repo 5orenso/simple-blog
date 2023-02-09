1887d33eed67f43f62aba6eb56f12ef3aee5b8c2
Oistein Sorensen <sorenso@gmail.com>
2023-02-09 15:54:01 +0100

    New livesenter build

--- preact/simple-blog-livecenter2/src/index.js
+++ preact/simple-blog-livecenter2/src/index.js
@@ -5,6 +5,6 @@ import Widget from "./app";
 let _habitat = habitat(Widget);
 
 _habitat.render({
-    selector: '[data-widget-host="simple-blog-livecenter"]',
+    selector: '[data-widget-host="simple-blog-livecenter2"]',
     clean: true
 });
