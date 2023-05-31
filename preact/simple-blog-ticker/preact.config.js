e69709e7dcdb1d283c0622c010663b18bd6e963d
Oistein Sorensen <sorenso@gmail.com>
2023-05-31 13:24:48 +0200

    New build

---
+++ preact/simple-blog-ticker/preact.config.js
@@ -0,0 +1,13 @@
+export default {
+    webpack(config, env, helpers, options) {
+        // config.entry = 'index.js';
+        // console.log({ env });
+        if (!env.isWatch) {
+            config.output = {
+                path: __dirname + '/bundle',
+                filename: '[name].js',
+                publicPath: '/preact/simple-blog-ticker/',
+            };
+        }
+	},
+};
